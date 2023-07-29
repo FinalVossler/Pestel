2e7342713e26798253e0c4e68e0c58f7f3b0b4ab
Hamza <hamza.khalifa.clashingames@gmail.com>
2023-07-29 21:44:00 +0200

    Added default props

--- src/components/pestel/Pestel.tsx
+++ src/components/pestel/Pestel.tsx
@@ -23,20 +23,55 @@ export interface IPestelTheme {
 }
 
 export interface IPestel {
-  theme: IPestelTheme;
-  data: IPestelScore[];
-  maxScore: number;
-  title: string;
-  downloadReportButtonText: string;
-  cancelButtonText: string;
-  confirmButtonText: string;
-  onCancel: () => void;
-  onConfirm: () => void;
-  productText: string;
-  countryText: string;
+  theme?: IPestelTheme;
+  data?: IPestelScore[];
+  maxScore?: number;
+  title?: string;
+  downloadReportButtonText?: string;
+  cancelButtonText?: string;
+  confirmButtonText?: string;
+  onCancel?: () => void;
+  onConfirm?: () => void;
+  productText?: string;
+  countryText?: string;
 }
 
-const Pestel: React.FunctionComponent<IPestel> = (props: IPestel) => {
+const defaultProps: IPestel = {
+  theme: {
+    borderColor: "#000000",
+    cancelButtonColor: "#FFFFFF",
+    cancelButtonTextColor: "#2C2B30",
+    confirmButtonLeftColor: "#2DB39E",
+    confirmButtonRightColor: "#4BE3AE",
+    confirmButtonTextColor: "#FFFFFF",
+    dotColor: "#3BCBB2",
+    downloadReportButtonColor: "#E59010",
+    downloadReportTextColor: "#FFFFFF",
+    textColor: "#2C2B30",
+    titleTextColor: "#2C2B30",
+    buttonBoxShadow: "0px 1px 4px rgba(0, 0, 0, 0.25)",
+  },
+  cancelButtonText: "Back",
+  confirmButtonText: "Confirm",
+  title: "PESTEL Analysis",
+  data: [
+    { score: 8, text: "Political" },
+    { score: 4, text: "Economic" },
+    { score: 10, text: "Social" },
+    { score: 6, text: "Technological" },
+    { score: 9, text: "Environmental" },
+    { score: 2, text: "Legal" },
+  ],
+  downloadReportButtonText: "Download Report",
+  maxScore: 10,
+  onCancel: () => {},
+  onConfirm: () => {},
+  productText: "Product Name: Product A",
+  countryText: "Country: France",
+};
+
+const Pestel: React.FunctionComponent<IPestel> = (passedProps: IPestel) => {
+  const props: IPestel = { ...defaultProps, ...passedProps };
   const styles = useStyles({ theme: props.theme });
 
   return (
@@ -61,7 +96,7 @@ const Pestel: React.FunctionComponent<IPestel> = (props: IPestel) => {
       </div>
 
       <div className={styles.scoresContainer}>
-        {props.data.map((score, scoreIndex) => {
+        {props.data?.map((score, scoreIndex) => {
           return (
             <div className={styles.scoreRow} key={scoreIndex}>
               <span className={styles.text}>{score.text}</span>
